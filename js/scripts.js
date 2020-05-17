2fd190ad3c56ace0f23d621ff624379f8de40566
MarcMartin <marcmartinsaumell@gmail.com>
2020-05-17 15:05:57 +0200

    subida 1

---
+++ js/scripts.js
@@ -0,0 +1,33 @@
+AOS.init();
+
+$('.js-menu').on('click', function() {
+    $('html').addClass('menu-open');
+});
+
+$('.js-closeMenu').on('click', function() {
+    $('html').removeClass('menu-open');
+});
+
+var swiper = new Swiper('.swiper-container', {
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+    pagination: {
+        el: '.swiper-pagination',
+    },
+});
+
+$(document).ready(function() {
+    $('a[href^="#"]').click(function() {
+      var destino = $(this.hash);
+      if (destino.length == 0) {
+        destino = $('a[name="' + this.hash.substr(1) + '"]');
+      }
+      if (destino.length == 0) {
+        destino = $('html');
+      }
+      $('html, body').animate({ scrollTop: destino.offset().top }, 500);
+      return false;
+    });
+  });
